129421ed1df04aba1a85a6a9cc475164a060001c
Justmoong <andudyun0504@gmail.com>
2025-06-24 18:54:07 +0900

    Initial Commit

---
+++ Time Scopes/Time_ScopesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ScopesApp.swift
+//  Time Scopes
+//
+//  Created by 윤무영 on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Time_ScopesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
